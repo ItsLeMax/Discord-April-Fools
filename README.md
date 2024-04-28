937d60ad0432a5f7caa91b8f12617bb5a6601639
ItsLeMax <lemaxyt1243@gmail.com>
2024-04-28 05:34:40 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# discord-april-fools-mass-rename
-Renames every person on a discord server to a chosable text.
+# Discord April Fools Mass Rename
+Code for a Discord Bot that renames every person on a discord server to a chosable text.
+
+> [!CAUTION]
+> This bot uses DiscordJS v13. Newer versions may be required and not work.
+
+## Requirements
+- NPM
+- Node
+- Alternatives may work aswell
+
+## Preparations
+You need to have a bot that runs the code. You can create one [here](https://discord.com/developers/applications).
+[This guide](https://discord.com/developers/docs/quick-start/getting-started) may be helpful.
+
+## How to set the bot up
+1. Download the source code of this repository.
+2. Go inside the root folder (where the package.json is located), open up a terminal (on windows you can do so by clicking into the file path and typing in `cmd` as seen in the attached image) and execute `npm i`.
+3. Open the .env file and put your bots token in there behind the equal sign.
+4. Go inside the `src` folder, open up the config.json and change every property as needed. The `command` property will cause specific actions to be performed. Change it step by step (every bot execution as seen in step 5) to one of the following:
+```
+0 - Default
+1 - Step 1: store old names
+2 - Step 2: execute (rename everyone)
+3 - Step 3: rollback (original names)
+404 - Completely wipe userdata.json
+```
+5. open up a terminal inside `src` and execute the command `node .` to run the bot.
---
+++ src/index.js
@@ -0,0 +1,95 @@
+/**
+ * @description
+ * Führt bestimmten Code aus.
+ * 
+ * Executes specific code.
+
+ * @example
+ * 0 = "Standard"
+ * 1 = "Schritt 1: Alte Namen speichern"
+ * 2 = "Schritt 2: Ausführen (alle umbenennen)"
+ * 3 = "Schritt 3: Urzustand wiederherstellen (Originalnamen)"
+ * 404 = "userdata.json vollständig zurücksetzen"
+ * 
+ * 0 = "Default"
+ * 1 = "Step 1: store old names"
+ * 2 = "Step 2: execute (rename everyone)"
+ * 3 = "Step 3: rollback (original names)"
+ * 404 = "Completely wipe userdata.json"
+
+ * @author ItsLeMax
+ */
+const level = config.command;
+
+const { Client, Intents } = require("discord.js");
+require("dotenv").config();
+const fs = require("fs");
+
+const intents = new Intents(32767); // excuse my lazy ass
+const client = new Client({ intents });
+
+client.login(process.env.TOKEN);
+
+const userdata = require("./userdata.json");
+const config = require("./config.json");
+
+client.on("ready", async () => {
+    const guild = client.guilds.cache.get(config.guild_id);
+    const members = await guild.members.fetch();
+
+    switch (level) {
+        case 1:
+            members.forEach(member => {
+                userdata.user.push({
+                    userId: member.user.id,
+                    username: member.displayName
+                });
+                fs.writeFile('./src/userdata.json', JSON.stringify(userdata, null, "\t"));
+            });
+
+            console.log("Die bisherigen Namen wurden erfolgreich gespeichert.", "Previous names were saved successfully.");
+            break;
+        case 2:
+            members.forEach(member => {
+                if (member.nickname != config.name) {
+                    console.log(`renaming ${member.nickname}...`);
+
+                    member.setNickname(config.name).catch(() => {
+                        console.warn(`couldn't rename ${member.nickname}.`);
+                    });
+                }
+            });
+
+            console.log("Die Umbenennung wurde beendet.", "The renaming has ended.");
+            break;
+        case 3:
+            for (const user of userdata.user) {
+                guildMember = await guild.members.fetch(user.userId);
+                console.log(`rolling back to ${user.username}...`);
+
+                guildMember.setNickname(user.username).catch(() => {
+                    console.warn(`couldn't rename back to ${user.username}.`);
+                });
+            }
+            break;
+        case 404:
+            userdata.user = [];
+            fs.writeFile('./src/userdata.json', JSON.stringify(userdata, null, "\t"));
+
+            console.log("Die Konfigurationsdatei wurde geleert.", "The configuration file has been wiped.");
+            break;
+        default:
+            console.log(
+                "Bitte ändere die Variable \"level\", damit der vorgesehene Codeblock ausgeführt werden kann.",
+                "Please change the variable \"level\" so that the proper code block can be executed."
+            );
+            break;
+    }
+
+    process.exit();
+})
+
+client.on("error", (error) => {
+    console.error(error);
+    process.exit();
+})
